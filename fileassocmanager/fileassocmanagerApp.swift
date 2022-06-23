3e0f26db891b93468f9e22c1c638107935a46def
Ruben Robles <d8vjork@outlook.com>
2022-06-23 21:25:42 +0200

    Initial Commit

---
+++ fileassocmanager/fileassocmanagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  fileassocmanagerApp.swift
+//  fileassocmanager
+//
+//  Created by Rubén Robles on 23/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct fileassocmanagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
